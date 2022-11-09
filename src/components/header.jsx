d21a45a95a441ae531b6cd60e35e59bc62f05b45
Sobiet <jablonski.m.jakub@gmail.com>
2022-11-09 11:46:27 +0100

    basic header design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,20 +3,22 @@ import Temp from "../images/temp.svg";
 
 function Header() {
   return (
-    <div className="justify-center w-full h-full flex flex-row bg-gray-600">
-      <div className=" w-full">
+    <div className="w-full h-full flex flex-row bg-gray-600 p-4">
+      <div className="w-1/3">
         <img src={Temp} alt="" className="h-16 w-16" />
       </div>
 
-      <div className="">
-        <img src={Temp} alt="" className="" />
+      <div className="w-1/3">
+        <div className="flex justify-center">
+          <img src={Temp} alt=""  />
+        </div>
       </div>
 
-      <div className="flex justify-self-end w-full">
-        <div className="flex flex-row">
+      <div className="w-1/3">
+        <div className="flex flex-row justify-end">
           <img src={Temp} alt="" className="h-16 w-16" />
 
-          <button className="">Log in/Sign in </button>
+          <button className="m-2 bg-gray-400 p-2 rounded-lg font-semibold hover:bg-gray-500">Sign in / Sign up </button>
         </div>
       </div>
     </div>
