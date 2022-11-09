fc8c65e2831b1ce18abb6ee45414f6cae94399a2
Sobiet <jablonski.m.jakub@gmail.com>
2022-11-09 11:17:48 +0100

    idk

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import Temp from "../images/temp.svg";
 
 function Header() {
-    return ( 
-        <div className="w-full h-full flex flex-row bg-gray-600">
-            <div>
-                <img src={Temp} alt="" />
-            </div>
+  return (
+    <div className="justify-center w-full h-full flex flex-row bg-gray-600">
+      <div className=" w-full">
+        <img src={Temp} alt="" className="h-16 w-16" />
+      </div>
 
-            <div>
-                <img src={Temp} alt="" />
-            </div>
+      <div className="">
+        <img src={Temp} alt="" className="" />
+      </div>
 
-            <div>
-                <img src={Temp} alt="" />
-                <button className="flex -end">Log in/Sign in </button>
-            </div>
+      <div className="flex justify-self-end w-full">
+        <div className="flex flex-row">
+          <img src={Temp} alt="" className="h-16 w-16" />
+
+          <button className="">Log in/Sign in </button>
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
