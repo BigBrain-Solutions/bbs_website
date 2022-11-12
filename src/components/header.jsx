f3753abd6fe72e7909492a7fbf71c86400cb52a2
Sobiet <jablonski.m.jakub@gmail.com>
2022-11-12 15:53:09 +0100

    frontend

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Temp from "../graphics/temp.svg";
-import User from "../graphics/user.svg";
+import Settings from "../graphics/settings.svg";
 import UserL from "../graphics/user_light.svg";
 
 function Header() {
   return (
-    <div className="w-full h-full flex flex-row bg-slate-800 p-2">
-      <div className="w-1/3">
-        <img src={Temp} alt="" className="h-12 w-12" />
+    <div className="w-full h-full flex flex-row bg-slate-800 p-1">
+      <div className="w-1/3 self-center">
+        <img
+          src={Settings}
+          alt=""
+          className="h-10 w-10 ml-1 cursor-pointer transition ease-in-out delay-75 hover:scale-105"
+        />
       </div>
 
       <div className="w-1/3">
@@ -18,10 +22,10 @@ function Header() {
 
       <div className="w-1/3">
         <div className="flex flex-row justify-end">
-          <img src={UserL} alt="" className="h-12 w-12 mt-1" />
+          <img src={UserL} alt="" className="h-10 w-10 self-center" />
 
-          <button className="m-2 bg-slate-400 p-2 rounded-lg font-semibold transition ease-in-out delay-75 hover:bg-slate-500 ">
-            Sign in / Sign up{" "}
+          <button className="m-2 bg-slate-400 p-2 text-sm rounded-lg font-semibold transition ease-in-out delay-75 hover:bg-slate-500 ">
+            Sign in / Sign up
           </button>
         </div>
       </div>
--- src/components/mainpage.jsx
+++ src/components/mainpage.jsx
@@ -1,39 +1,59 @@
 import React from "react";
-import Forest from "../graphics/foggy_forest.jpg";
+// import Forest from "../graphics/foggy_forest.jpg";
 import User from "../graphics/user.svg";
 
 function MainPage() {
   return (
     <div className="place-items-center flex flex-col mt-10 bg-slate-700">
       <div className="w-2/4 p-2 m-4 flex flex-row rounded-2xl bg-slate-900 cursor-pointer transition ease-in-out delay-75 shadow-sm hover:shadow-xl hover:shadow-slate-800 hover:scale-105">
-        <img src={User} alt="" className="flex justify-sefl-start"/>
+        <img src={User} alt="" className="flex justify-sefl-start" />
         <div className="p-2">
-          <div className="text-white font-semibold text-lg">This is a title</div>
-          <div className="text-white text-sm ">This is a short descrioption written by me the creator of the website</div>
+          <div className="text-white font-semibold text-lg">
+            This is a title
+          </div>
+          <div className="text-white text-sm ">
+            This is a short descrioption written by me the creator of the
+            website
+          </div>
         </div>
       </div>
 
       <div className="w-2/4 p-2 m-4 flex flex-row rounded-2xl bg-slate-900 cursor-pointer transition ease-in-out delay-75 shadow-sm hover:shadow-xl hover:shadow-slate-800 hover:scale-105">
-        <img src={User} alt="" className="flex justify-sefl-start"/>
+        <img src={User} alt="" className="flex justify-sefl-start" />
         <div className="p-2">
-          <div className="text-white font-semibold text-lg">This is a title</div>
-          <div className="text-white text-sm ">This is a short descrioption written by me the creator of the website</div>
+          <div className="text-white font-semibold text-lg">
+            This is a title
+          </div>
+          <div className="text-white text-sm ">
+            This is a short descrioption written by me the creator of the
+            website
+          </div>
         </div>
       </div>
 
       <div className="w-2/4 p-2 m-4 flex flex-row rounded-2xl bg-slate-900 cursor-pointer transition ease-in-out delay-75 shadow-sm hover:shadow-xl hover:shadow-slate-800 hover:scale-105">
-        <img src={User} alt="" className="flex justify-sefl-start"/>
+        <img src={User} alt="" className="flex justify-sefl-start" />
         <div className="p-2">
-          <div className="text-white font-semibold text-lg">This is a title</div>
-          <div className="text-white text-sm ">This is a short descrioption written by me the creator of the website</div>
+          <div className="text-white font-semibold text-lg">
+            This is a title
+          </div>
+          <div className="text-white text-sm ">
+            This is a short descrioption written by me the creator of the
+            website
+          </div>
         </div>
       </div>
 
       <div className="w-2/4 p-2 m-4 flex flex-row rounded-2xl bg-slate-900 cursor-pointer transition ease-in-out delay-75 shadow-sm hover:shadow-xl hover:shadow-slate-800 hover:scale-105">
-        <img src={User} alt="" className="flex justify-sefl-start"/>
+        <img src={User} alt="" className="flex justify-sefl-start" />
         <div className="p-2">
-          <div className="text-white font-semibold text-lg">This is a title</div>
-          <div className="text-white text-sm ">This is a short descrioption written by me the creator of the website</div>
+          <div className="text-white font-semibold text-lg">
+            This is a title
+          </div>
+          <div className="text-white text-sm ">
+            This is a short descrioption written by me the creator of the
+            website
+          </div>
         </div>
       </div>
 
